2aebac14889a889bb88b03a1695808600df61645
DiestroCorp <matias.perez.mcd@gmail.com>
2023-02-04 02:30:09 -0300

    Code optimized for use with GitHub Pages.

--- js/main.js
+++ js/main.js
@@ -249,7 +249,8 @@ function borrarConsultas() {
 
 // Hacemos Fetch al JSON en cuanto carga el documento.
 $(document).ready(function () {
-  fetch("http://127.0.0.1:5500/viajes/js/data.json")
+  fetch("https://diestrocorleone.github.io/viajes-reloaded/js/data.json")
+    //fetch("http://127.0.0.1:5500/viajes/js/data.json") SOLO FUNCIONA EN LOCAL CON VSCODE
     .then((response) => response.json())
     .then((json) => {
       json.destinations.map(
